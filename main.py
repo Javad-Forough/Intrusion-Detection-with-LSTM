7f4efd0ebcd730a03fd639239a2fd15a808f306b
Javad <javadforough@gmail.com>
2024-03-07 11:43:07 +0100

    UPDATE main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import data_preprocessing
 import model
+import hyperparameter_tuning
 
 if __name__ == "__main__":
-    train_file = "Benchmark/UNSW_NB15_training-set.csv"
-    test_file = "Benchmark/UNSW_NB15_testing-set.csv"
+    training = "UNSW_NB15_training-set.csv"
+    testing = "UNSW_NB15_testing-set.csv"
     
-    x_train, y_train, x_test, y_test = data_preprocessing.read_data(train_file, test_file)
+    x_train, y_train, x_test, y_test = data_preprocessing.read_data(training, testing)
     x_train = data_preprocessing.scale_data(x_train)
     x_test = data_preprocessing.scale_data(x_test)
+    
+    hyperparameter_tuning.hyperparameter_tuning(x_train, y_train)
     
     input_shape = (7, 38)
     units = 32
@@ -15,5 +18,13 @@ if __name__ == "__main__":
     batch_size = 32
     
     lstm_model = model.build_lstm_model(input_shape, units)
-    model.train_model(lstm_model, x_train, y_train, epochs, batch_size)
-    model.evaluate_model(lstm_model, x_test, y_test)
+    train_time = model.train_model(lstm_model, x_train, y_train, epochs, batch_size)
+    prec, recall, f_1, acc, myauc, mypr = model.evaluate_model(lstm_model, x_test, y_test)
+    
+    print("Train Time:", train_time)
+    print("Precision:", prec)
+    print("Recall:", recall)
+    print("F1 Score:", f_1)
+    print("Accuracy:", acc)
+    print("AUC:", myauc)
+    print("Average Precision:", mypr)
